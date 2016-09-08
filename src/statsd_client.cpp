13ecaf463a729c1bea4924f97ac05a99570fa55f
Ivan Baidakou <basiliscos@openmailbox.org>
2016-09-08 20:38:26 +0300

    Patches to let it compiles on windows

--- src/statsd_client.cpp
+++ src/statsd_client.cpp
@@ -1,15 +1,33 @@
-
 #include <math.h>
-#include <netdb.h>
 #include <time.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include "statsd_client.h"
-#include <fcntl.h>
+
+#ifdef _WIN32
+  /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
+  #ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0501  /* Windows XP. */
+  #endif
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+#else
+  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+  #include <unistd.h> /* Needed for close() */
+  #include <netinet/in.h>
+  #include <fcntl.h>
+#endif
+
+#ifdef _WIN32
+    #define CLOSE_SOCKET(s) closesocket(s)
+#else
+    #define CLOSE_SOCKET(s) close(s)
+#endif
 
 using namespace std;
 namespace statsd {
@@ -27,7 +45,7 @@ inline bool should_send(float sample_rate)
         return true;
     }
 
-    float p = ((float)random() / RAND_MAX);
+    float p = ((float)rand() / RAND_MAX);
     return sample_rate > p;
 }
 
@@ -48,14 +66,14 @@ StatsdClient::StatsdClient(const string& host, int port, const string& ns)
     d = new _StatsdClientData;
     d->sock = -1;
     config(host, port, ns);
-    srandom(time(NULL));
+    srand(time(NULL));
 }
 
 StatsdClient::~StatsdClient()
 {
     // close socket
     if (d->sock >= 0) {
-        close(d->sock);
+        CLOSE_SOCKET(d->sock);
         d->sock = -1;
         delete d;
         d = NULL;
@@ -69,7 +87,7 @@ void StatsdClient::config(const string& host, int port, const string& ns)
     d->port = port;
     d->init = false;
     if ( d->sock >= 0 ) {
-        close(d->sock);
+        CLOSE_SOCKET(d->sock);
     }
     d->sock = -1;
 }
@@ -88,25 +106,21 @@ int StatsdClient::init()
     d->server.sin_family = AF_INET;
     d->server.sin_port = htons(d->port);
 
-    int ret = inet_aton(d->host.c_str(), &d->server.sin_addr);
-    if ( ret == 0 )
-    {
-        // host must be a domain, get it from internet
-        struct addrinfo hints, *result = NULL;
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_DGRAM;
+    // host must be a domain, get it from internet
+    struct addrinfo hints, *result = NULL;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
 
-        ret = getaddrinfo(d->host.c_str(), NULL, &hints, &result);
-        if ( ret ) {
-            snprintf(d->errmsg, sizeof(d->errmsg),
-                    "getaddrinfo fail, error=%d, msg=%s", ret, gai_strerror(ret) );
-            return -2;
-        }
-        struct sockaddr_in* host_addr = (struct sockaddr_in*)result->ai_addr;
-        memcpy(&d->server.sin_addr, &host_addr->sin_addr, sizeof(struct in_addr));
-        freeaddrinfo(result);
+    int ret = getaddrinfo(d->host.c_str(), NULL, &hints, &result);
+    if ( ret ) {
+        snprintf(d->errmsg, sizeof(d->errmsg),
+                "getaddrinfo fail, error=%d, msg=%s", ret, gai_strerror(ret) );
+        return -2;
     }
+    struct sockaddr_in* host_addr = (struct sockaddr_in*)result->ai_addr;
+    memcpy(&d->server.sin_addr, &host_addr->sin_addr, sizeof(struct in_addr));
+    freeaddrinfo(result);
 
     d->init = true;
     return 0;
